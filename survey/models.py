3ab90eecc0bcb182f72ebf7c0e853904b2c1e1ad
yossvalencia98 <70604907+yossvalencia98@users.noreply.github.com>
2020-09-08 17:59:19 -0500

    Add files via upload
    primera programacion

---
+++ survey/models.py
@@ -0,0 +1,99 @@
+from otree.api import (
+    models,
+    widgets,
+    BaseConstants,
+    BaseSubsession,
+    BaseGroup,
+    BasePlayer,
+    Currency as c,
+    currency_range,
+)
+
+
+class Constants(BaseConstants):
+    name_in_url = 'survey'
+    players_per_group = 4
+    num_rounds = 1
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    urna1 = models.StringField(
+        choices=[['A', 'A'], ['B', 'B']],
+        label='¿que bola le corresponde?',
+        widget=widgets.RadioSelect,
+    )
+
+    urna2 = models.StringField(
+        choices=[['A', 'A'], ['B', 'B']],
+        label='¿que bola le corresponde?',
+        widget=widgets.RadioSelect,
+    )
+
+    urna3 = models.StringField(
+        choices=[['A', 'A'], ['B', 'B']],
+        label='¿que bola le corresponde?',
+        widget=widgets.RadioSelect,
+    )
+
+    urna4 = models.StringField(
+        choices=[['A', 'A'], ['B', 'B']],
+        label='¿que bola le corresponde?',
+        widget=widgets.RadioSelect,
+    )
+
+class Player(BasePlayer):
+
+    age = models.IntegerField(label='Edad:', min=18, max=90)
+
+    gender = models.IntegerField(
+        choices=[[0, 'Hombre'], [1, 'Mujer']],
+        label='Género:',
+        widget=widgets.RadioSelect,
+    )
+    escala = models.IntegerField(
+        choices=[
+            [0, 'Becado'],
+            [2, 'Escala 1 (antigua)'],
+            [4, 'Escala 2 (antigua)'],
+            [6, 'Escala 3 (antigua)'],
+            [9, 'Escala 4 (antigua)'],
+            [13, 'Escala 5 (antigua)'],
+            [1, 'Escala 1 (nueva)'],
+            [3, 'Escala 2 (nueva)'],
+            [5, 'Escala 3 (nueva)'],
+            [7, 'Escala 4 (nueva)'],
+            [8, 'Escala 5 (nueva)'],
+            [10, 'Escala 6 (nueva)'],
+            [11, 'Escala 7 (nueva)'],
+            [12, 'Escala 8 (nueva)'],
+            [14, 'Escala 9 (nueva)'],
+        ],
+        label='¿En qué escala de pago te encuentras?',
+    )
+
+    loterias = models.IntegerField(
+        choices=[
+            [0, 'Lotería 1'],
+            [1, 'Lotería 2 '],
+            [2, 'Lotería 3 '],
+            [3, 'Lotería 4 '],
+            [4, 'Lotería 5 '],
+            [5, 'Lotería 6 '],
+
+        ],
+        label='¿Qué lotería prefiere?',
+    )
+
+    urnaprueba = models.StringField(
+        choices=[['A', 'A'], ['B', 'B']],
+        label='¿A que urna crees corresponde esta esfera?',
+        widget=widgets.RadioSelect,
+    )
+
+    urnapago = models.IntegerField(min=5, max=5,    label='De haber acertado cuanto le correspondería',    )
+
+    acepta = models.StringField(choices=['No', 'Sí'], widget=widgets.RadioSelectHorizontal,)
---
+++ survey/pages.py
@@ -0,0 +1,70 @@
+from otree.api import Currency as c, currency_range
+
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+
+class Demografico(Page):
+    form_model = 'player'
+    form_fields = ['age', 'gender', 'escala', 'loterias']
+
+
+
+class instrucciones(Page):
+    pass
+
+class instrucciones2(Page):
+    pass
+
+class Prueba (Page):
+    form_model = 'player'
+    form_fields = ['urnaprueba', 'urnapago' ]
+
+
+class NormalWaitPage(WaitPage):
+    pass
+
+class ejercicio1 (Page):
+    form_model = 'group'
+    form_fields = ['urna1']
+
+    def is_displayed(self):
+        return self.player.id_in_group == 1
+
+
+class ejercicio2 (Page):
+    form_model = 'group'
+    form_fields = ['urna2']
+
+    def is_displayed(self):
+        return self.player.id_in_group == 2
+
+class ejercicio3 (Page):
+    form_model = 'group'
+    form_fields = ['urna3']
+
+    def is_displayed(self):
+        return self.player.id_in_group == 3
+
+class ejercicio4 (Page):
+    form_model = 'group'
+    form_fields = ['urna4']
+
+    def is_displayed(self):
+        return self.player.id_in_group == 4
+
+class Pagos (Page):
+    pass
+
+
+class ConsentimientoInformado(Page):
+    form_model = 'player'
+    form_fields = ['acepta']
+
+
+class agradecimiento(Page):
+    def is_displayed(self):
+        return self.player.acepta =="no"
+
+
+page_sequence = [ConsentimientoInformado, agradecimiento, instrucciones, Prueba, ejercicio1, NormalWaitPage, ejercicio2, NormalWaitPage, ejercicio3, NormalWaitPage, ejercicio4, NormalWaitPage, Demografico, Pagos ]
