f37a6d712dfa15574f0e685153be6ad4c4d719a3
yossvalencia98 <70604907+yossvalencia98@users.noreply.github.com>
2020-09-15 17:20:58 -0500

    Add files via upload

--- survey/pages.py
+++ survey/pages.py
@@ -8,18 +8,8 @@ class Demografico(Page):
     form_model = 'player'
     form_fields = ['age', 'gender', 'escala', 'loterias']
 
-
-
-class instrucciones(Page):
-    pass
-
 class instrucciones2(Page):
     pass
-
-class Prueba (Page):
-    form_model = 'player'
-    form_fields = ['urnaprueba', 'urnapago' ]
-
 
 class NormalWaitPage(WaitPage):
     pass
@@ -78,7 +68,7 @@ class ejercicio5 (Page):
             self.player.payoff = self.player.payoff + 5
 
     def is_displayed(self):
-        return self.player.id_in_group == 1
+        return self.player.id_in_group == 5
 
 
 class ejercicio6 (Page):
@@ -90,7 +80,7 @@ class ejercicio6 (Page):
             self.player.payoff = self.player.payoff + 5
 
     def is_displayed(self):
-        return self.player.id_in_group == 2
+        return self.player.id_in_group == 6
 
 class ejercicio7 (Page):
     form_model = 'group'
@@ -101,7 +91,7 @@ class ejercicio7 (Page):
             self.player.payoff = self.player.payoff + 5
 
     def is_displayed(self):
-        return self.player.id_in_group == 3
+        return self.player.id_in_group == 7
 
 class ejercicio8 (Page):
     form_model = 'group'
@@ -112,22 +102,106 @@ class ejercicio8 (Page):
             self.player.payoff = self.player.payoff + 5
 
     def is_displayed(self):
+        return self.player.id_in_group == 8
+
+class ejercicio9 (Page):
+    form_model = 'group'
+    form_fields = ['urna9']
+
+    def before_next_page(self):
+        if self.group.urna9 == self.group.myrandom:
+            self.player.payoff = self.player.payoff + 5
+
+    def is_displayed(self):
+        return self.player.id_in_group == 1
+
+class ejercicio10 (Page):
+    form_model = 'group'
+    form_fields = ['urna10']
+
+    def before_next_page(self):
+        if self.group.urna10 == self.group.myrandom:
+            self.player.payoff = self.player.payoff + 5
+
+    def is_displayed(self):
+        return self.player.id_in_group == 2
+class ejercicio11 (Page):
+    form_model = 'group'
+    form_fields = ['urna11']
+
+    def before_next_page(self):
+        if self.group.urna8 == self.group.myrandom:
+            self.player.payoff = self.player.payoff + 5
+
+    def is_displayed(self):
+        return self.player.id_in_group == 3
+class ejercicio12 (Page):
+    form_model = 'group'
+    form_fields = ['urna12']
+
+    def before_next_page(self):
+        if self.group.urna12 == self.group.myrandom:
+            self.player.payoff = self.player.payoff + 5
+
+    def is_displayed(self):
         return self.player.id_in_group == 4
+class ejercicio13 (Page):
+    form_model = 'group'
+    form_fields = ['urna13']
+
+    def before_next_page(self):
+        if self.group.urna13 == self.group.myrandom:
+            self.player.payoff = self.player.payoff + 5
+
+    def is_displayed(self):
+        return self.player.id_in_group == 5
+class ejercicio14 (Page):
+    form_model = 'group'
+    form_fields = ['urna14']
+
+    def before_next_page(self):
+        if self.group.urna14 == self.group.myrandom:
+            self.player.payoff = self.player.payoff + 5
+
+    def is_displayed(self):
+        return self.player.id_in_group == 4
+class ejercicio15 (Page):
+    form_model = 'group'
+    form_fields = ['urna15']
+
+    def before_next_page(self):
+        if self.group.urna15 == self.group.myrandom:
+            self.player.payoff = self.player.payoff + 5
+
+    def is_displayed(self):
+        return self.player.id_in_group == 4
+class ejercicio16 (Page):
+    form_model = 'group'
+    form_fields = ['urna16']
+
+    def before_next_page(self):
+        if self.group.urna16 == self.group.myrandom:
+            self.player.payoff = self.player.payoff + 5
+
+    def is_displayed(self):
+        return self.player.id_in_group == 4
+
+
+
+
+
+
 
 class Pagos (Page):
     pass
 
 
-class ConsentimientoInformado(Page):
-    form_model = 'player'
-    form_fields = ['acepta']
 
 
-class agradecimiento(Page):
-    def is_displayed(self):
-        return self.player.acepta =="no"
+
+
 
 class Resultados(Page):
     pass
 
-page_sequence = [ConsentimientoInformado, agradecimiento, instrucciones, Prueba, ejercicio1, NormalWaitPage, ejercicio2, NormalWaitPage, ejercicio3, NormalWaitPage, ejercicio4, NormalWaitPage, instrucciones2, ejercicio5, NormalWaitPage, ejercicio6, NormalWaitPage, ejercicio7, NormalWaitPage, ejercicio8, NormalWaitPage, Demografico, Resultados, Pagos ]
+page_sequence = [ejercicio1, NormalWaitPage, ejercicio2, NormalWaitPage, ejercicio3, NormalWaitPage, ejercicio4, NormalWaitPage, instrucciones2, ejercicio5, NormalWaitPage, ejercicio6, NormalWaitPage, ejercicio7, NormalWaitPage, ejercicio8, NormalWaitPage, Demografico, Resultados, Pagos ]
